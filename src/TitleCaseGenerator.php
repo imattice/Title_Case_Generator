266d65fc9c7029966f5fd4789037328203a1fbd5
Ike Mattice & Deron Johnson <student@epicodus.com>
2015-08-10 14:48:03 -0700

    Added functionality for O'Mallet

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -4,21 +4,36 @@
     {
         function makeTitleCase($input_title)
         {
-            $input_array_of_words = explode(' ', $input_title);
+            //seperates the input into seperate words by looking for empty spaces
+            $user_input = strtolower($input_title);
+            $input_array_of_words = explode(' ', $user_input);
             $output_titlecased = array();
             foreach ($input_array_of_words as $word)
             {
+                /*function checkForMcChicken(
+                    $count = 0;
+                    foreach (
+                ) */
                 $conjunction = array('a', 'an', 'the', 'and', 'of', 'but', 'or', 'for', 'nor', 'with', 'on', 'at', 'to', 'from', 'by');
 
-                if(in_array(strtolower($word), $conjunction))
+                if(in_array($word, $conjunction))
                 {
+                    //checks if $conjunction is at the beginning and capitalizes it
                     if ($word == $input_array_of_words[0]) {
                         array_push($output_titlecased, ucfirst($word));
+                    //if $conjuction is not at the beginning, it is not capitalized
                     } else {
                         array_push($output_titlecased, lcfirst($word));
                     }
 
+                } elseif (strpos($word, "'")) {
+                    /* if ($word[strlen($word)-1]) {
+                        array_push($output_titlecased, lcfirst($word));
+                    } else {*/
+                    array_push($output_titlecased, ucwords($word, "'"));
+                    //}
                 } else {
+                    //any word not in $conjunction is capitalized
                     array_push($output_titlecased, ucfirst($word));
                 }
 
