06547961ce8895995ae6e9c26fecc9a391d15c3f
Ike Mattice & Deron Johnson <student@epicodus.com>
2015-08-10 11:25:00 -0700

    Edit class to handle first word conjunctions.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -9,18 +9,22 @@
             foreach ($input_array_of_words as $word)
             {
                 $conjunction = array('a', 'an', 'the', 'and', 'of', 'but', 'or', 'for', 'nor', 'with', 'on', 'at', 'to', 'from', 'by');
-                
+
                 if(in_array($word, $conjunction))
                 {
                     //add lower case function here before the $word
-                    array_push($output_titlecased, $word);
+                    if ($word == $input_array_of_words[0]) {
+                        array_push($output_titlecased, ucfirst($word));
+                    } else {
+                        array_push($output_titlecased, $word);
+                    }
+
                 } else {
                     array_push($output_titlecased, ucfirst($word));
                 }
 
             }
             return implode (' ', $output_titlecased);
-
         }
     }
 ?>
